9d9b887464f41ba4a680706f4b5fa2f05d6aa86a
Marco Pisco <marco@marcopisco.com>
2025-01-30 20:31:15 +0000

    Update ao novo sistema de formulários

--- formlist/dev.php
+++ formlist/dev.php
@@ -4,8 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link href="/css/main.css" rel="stylesheet">
 </head>
 <body>
+<br>
+<div class='h-100 d-flex align-items-center justify-content-center flex-column'>
+<p class='h2 mb-4'>Preencha o Formulário</p>
+<p class='mb-4'>Este formulário obriga o preenchimento dos seguintes campos:</p>
 <form action="doc.php" method="post">
 <?php
     ini_set('display_errors', 1);
@@ -17,7 +24,7 @@
         echo "<div class='mb-3'>";
         echo "<label for='{$quest->idcampo}' class='form-label'>{$quest->descricao}";
         if ($quest->obrigatorio){
-            echo "<b class='required'>*</b>";
+            echo " <b class='required'>*</b>";
         };
         echo ":</label>";
         echo "<input type='{$quest->tipo}' class='form-control' id='{$quest->idcampo}' name='{$quest->idcampo}'";
