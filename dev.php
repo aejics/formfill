98449440932bee19eed3e6ef5d265cffb10cf179
Marco Pisco <a11531@aejics.org>
2025-01-31 21:30:37 +0000

    Finalizar novo código de formulário

--- dev.php
+++ dev.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
     <link href="/css/main.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
@@ -12,6 +11,9 @@
 <br>
 <div class='h-100 d-flex align-items-center justify-content-center flex-column'>
 <?php
+    // Caso não esteja especificado correctamente o ID do formulário (por ex. ?formid=1),
+    // matar o script e dar um erro ao utilizador.
+    // Esta parte do código foi implementada de maneira a 
     if (!$_GET["formid"]){
         die("<div class='alert alert-danger text-center' role='alert'>
         O formulário não está especificado. Por favor, contacte o administrador.
@@ -19,15 +21,20 @@
     }
 ?>
 <?php
+    // Importar configurações do formulário
     $formulario = json_decode(file_get_contents("formlist/" . $_GET["formid"] . ".json"));
+    // Caso o formulário não exista, matar o script e dar um erro ao utilizador.
     if (!$formulario){
         die("<div class='alert alert-danger text-center' role='alert'>
         O formulário não existe. Por favor, contacte o administrador.
         </div>");
     }
-    echo "<p class='h2 mb-4'>{$formulario->nome}</p>";
-    echo "<p class='mb-4'>{$formulario->subnomePreenchimento}</p>";
+    echo "<title>{$formulario->nome} - FormFill</title>"; // Dar título à página
+    echo "<p class='h2 mb-4'>{$formulario->nome}</p>"; // Mostrar título na página
+    echo "<p class='mb-4'>{$formulario->subnomePreenchimento}</p>"; // Descrição do formulário para o utilizador
     echo "<form action='doc.php' method='post'><input type='hidden' name='formid' value='{$_GET["formid"]}'>";
+    // ^ Função HTML para devolver o formulário ao script de documento.
+    // Cada campo do formulário gera um label.
     foreach ($formulario->campos as $index=>$quest){
         echo "<div class='mb-3'>";
         echo "<label for='{$quest->idcampo}' class='form-label'>{$quest->descricao}";
@@ -41,6 +48,7 @@
         }
         echo "></div>";
     }
+    // Botão que tem de obrigatoriamente estar no forumlário para submeter.
     echo "<button type='submit' class='btn btn-primary w-100'>Submeter</button>";
 ?>    
 </body>
