d1bc52d9b64a5460940e55b64b87812759f12a63
Marco Pisco <a11531@aejics.org>
2025-01-31 21:44:09 +0000

    A minha retardação mental está a começar outra vez. Este código não é para verificar (mas teria sido uma ótima ideia ter feito isso!)

--- src/header.php
+++ src/header.php
@@ -13,19 +13,19 @@
   echo "<nav class='navbar navbar-expand-lg navbar-light bg-light justify-content-center'>
   <a class='navbar-brand' href='/'>FormFill</a>
   <div class='dropdown'>";
-  // if (isset($_COOKIE["loggedin"])){
-  //   require_once(__DIR__ . "/../vendor/autoload.php");
-  //   $giae = new \juoum\GiaeConnect\GiaeConnect("giae.aejics.org");
-  //   $giae->session=$_COOKIE["session"];
-  //   $config = json_decode($giae->getConfInfo(), true);
-  //   $fotoutente = json_decode('"' . $config['fotoutente'] . '"'); // Decode Unicode da Foto do Utente
-  //   echo "<button class='btn btn-secondary dropdown-toggle' type='button' id='areaMenuButton' data-bs-toggle='dropdown' aria-expanded='false'>
-  //     <img class='fotoutente' src='https://giae.aejics.org/" . $fotoutente . "'>  A Minha Área
-  //     </button>
-  //     <ul class='dropdown-menu' aria-labelledby='dropdownMenuButton'>
-  //     <li><a class='dropdown-item' href='/logout.php'>Terminar sessão</a></li>
-  //   </ul>
-  //   </div>";
-  // }
+  if (isset($_COOKIE["loggedin"])){
+    require_once(__DIR__ . "/../vendor/autoload.php");
+    $giae = new \juoum\GiaeConnect\GiaeConnect("giae.aejics.org");
+    $giae->session=$_COOKIE["session"];
+    $config = json_decode($giae->getConfInfo(), true);
+    $fotoutente = json_decode('"' . $config['fotoutente'] . '"'); // Decode Unicode da Foto do Utente
+    echo "<button class='btn btn-secondary dropdown-toggle' type='button' id='areaMenuButton' data-bs-toggle='dropdown' aria-expanded='false'>
+      <img class='fotoutente' src='https://giae.aejics.org/" . $fotoutente . "'>  A Minha Área
+      </button>
+      <ul class='dropdown-menu' aria-labelledby='dropdownMenuButton'>
+      <li><a class='dropdown-item' href='/logout.php'>Terminar sessão</a></li>
+    </ul>
+    </div>";
+  }
   echo "</nav>";
 ?>
