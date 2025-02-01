b8715db07501cbd2426f77509b69372ccda53fc3
Marco Pisco <a11531@aejics.org>
2025-02-01 00:49:45 +0000

    Implementar SQLite com login. Começar a implementar bases de dados no programa.

--- dblogin.php
+++ dblogin.php
@@ -14,15 +14,21 @@
     <br>
     <div class='h-100 d-flex align-items-center justify-content-center flex-column'>
 <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
     require_once(__DIR__ . "/vendor/autoload.php");
-    if (!isset($_COOKIE["loggedin"]) && $_GET["action"] !== "loginform" && $_GET["action"] !== "login"){
-        header('Location: /login.php?action=loginform');
-    };
-    if ($_GET["action"] == "loginform"){
+    $db = new SQLite3('db.sqlite3');
+    $action = filter_input(INPUT_GET, 'action', FILTER_UNSAFE_RAW);
+    $loggedin = filter_input(INPUT_COOKIE, 'loggedin', FILTER_UNSAFE_RAW);
+    if (!$loggedin && $action !== "loginform" && $action !== "login"){
+        header('Location: /dblogin.php?action=loginform');
+    }
+    if ($action == "loginform"){
         print("<div class='h-100 d-flex align-items-center justify-content-center flex-column'>
             <p class='h2 mb-4'>Autentique-se via GIAE AEJICS</p>
             <p class='mb-4'>Utilize as credenciais do GIAE AEJICS para continuar para <b>FormFill</b></p>
-            <form action='/login.php?action=login' method='POST' class='w-200' style='max-width: 600px;'>
+            <form action='/dblogin.php?action=login' method='POST' class='w-200' style='max-width: 600px;'>
                 <div class='mb-3'>
                     <label for='user' class='form-label'>Nome de utilizador <b class='required'>*</b>:</label>
                     <input type='text' class='form-control' id='user' name='user' required autofocus placeholder='fxxxx ou axxxxx'>
@@ -38,10 +44,13 @@
             <hr>
         </div>");
     }
-    if ($_GET["action"] == "login"){
-        $giae = new \juoum\GiaeConnect\GiaeConnect("giae.aejics.org", $_POST["user"], $_POST["pass"]);
+    if ($action == "login"){
+        $user = filter_input(INPUT_POST, 'user', FILTER_UNSAFE_RAW);
+        $pass = filter_input(INPUT_POST, 'pass', FILTER_UNSAFE_RAW);
+        $giae = new \juoum\GiaeConnect\GiaeConnect("giae.aejics.org", $user, $pass);
         $config = json_decode($giae->getConfInfo(), true);
-        if (str_contains($giae->getConfInfo(), 'Erro do Servidor')){
+        $perfil = json_decode($giae->getPerfil(), true);
+        if (strpos($giae->getConfInfo(), 'Erro do Servidor') !== false){
             echo("<div class='alert alert-danger text-center' role='alert'>A sua palavra-passe está errada.</div>
             <div class='text-center'>
             <button type='button' class='btn btn-primary w-100' onclick='history.back()'>Voltar</button></div>");
@@ -50,20 +59,27 @@
             setcookie("loggedin", "true", time() + 3599, "/");
             setcookie("session", $giae->session, time() + 3599, "/");
             setcookie("user", $_POST["user"], time() + 3599, "/");
+            $valordb = $db->prepare("INSERT INTO cache_giae(id, nome, nomecompleto, email) VALUES (:1, :2, :3, :4);");
+            $valordb->bindValue(':1', $_POST["user"], SQLITE3_TEXT);
+            $valordb->bindValue(':2', $config['nomeutilizador'], SQLITE3_TEXT);
+            $valordb->bindValue(':3', $perfil['perfil']['nome'], SQLITE3_TEXT);
+            $valordb->bindValue(':4', $perfil['perfil']['email'], SQLITE3_TEXT);
+            $valordb->execute();
             header('Location: /');
         }
     };
-    if (isset($_COOKIE["loggedin"])){
+    if ($loggedin){
+        $giae->session = filter_input(INPUT_COOKIE, 'session', FILTER_UNSAFE_RAW);
         $giae = new \juoum\GiaeConnect\GiaeConnect("giae.aejics.org");
         $giae->session=$_COOKIE["session"];
         // Este código funciona especificamente com a maneira de verificação no GIAE AEJICS.
         // Pode não funcionar da mesma maneira nos outros GIAEs. Caso não funcione na mesma maneira, corriga este código e faça um pull request!
         if (str_contains($giae->getConfInfo(), 'Erro do Servidor')){
-            header('Location: /login.php?action=logout');
+            header('Location: /dblogin.php?action=logout');
             die("A sua sessão expirou");
         }
-    };
-    if ($_GET["action"] == "logout"){
+    }
+    if ($action == "logout"){
         $giae = new \juoum\GiaeConnect\GiaeConnect("giae.aejics.org");
         $giae->session=$_COOKIE["session"];
         $giae->logout();
@@ -71,6 +87,6 @@
         echo("<div class='alert alert-success text-center' role='alert'>A sua sessão foi terminada com sucesso.</div>
         <div class='text-center'>
         <button type='button' class='btn btn-primary w-100' onclick='history.back()'>Voltar</button></div>");
-    require 'src/footer.php';
     };
+    require 'src/footer.php';
 ?>
