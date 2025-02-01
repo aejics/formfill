1bbc08b66be9f9dca36469b9867e3f8c942a6ba1
Marco Pisco <a11531@aejics.org>
2025-02-01 22:13:50 +0000

    é filled, não devtemp

--- form.php
+++ form.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/css/main.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</head>
-<body>
-<br>
-<div class='h-100 d-flex align-items-center justify-content-center flex-column'>
+<?php require('login.php'); ?>
+<link href="/assets/main.css" rel="stylesheet">
 <?php
     // Caso não esteja especificado correctamente o ID do formulário (por ex. ?formid=1),
     // matar o script e dar um erro ao utilizador.
@@ -32,7 +22,7 @@
     echo "<title>{$formulario->nome} - FormFill</title>"; // Dar título à página
     echo "<p class='h2 mb-4'>{$formulario->nome}</p>"; // Mostrar título na página
     echo "<p class='mb-4'>{$formulario->subnomePreenchimento}</p>"; // Descrição do formulário para o utilizador
-    echo "<form action='devtemp.php' method='post'><input type='hidden' name='formid' value='{$_GET["formid"]}'>";
+    echo "<form action='filled.php' method='post'><input type='hidden' name='formid' value='{$_GET["formid"]}'>";
     // ^ Função HTML para devolver o formulário ao script de documento.
     // Cada campo do formulário gera um label.
     foreach ($formulario->campos as $index=>$quest){
