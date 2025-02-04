6a6383a13d7c0dbb3bb34d5c21bceef9224ed1c6
Marco Pisco <a11531@aejics.org>
2025-02-04 15:15:07 +0000

    Usar branch do main para determinar a versão

--- src/footer.php
+++ src/footer.php
@@ -1,6 +1,6 @@
 <?php 
 function getLatestCommit() {
-    return substr(file_get_contents(sprintf( '.git/refs/heads/%s', 'dev' )),0,8);
+    return substr(file_get_contents(sprintf( '.git/refs/heads/%s', 'main' )),0,8);
 }
 
 echo "<hr><div class='h-100 d-flex align-items-center justify-content-center flex-column'>
